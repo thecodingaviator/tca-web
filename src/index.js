986b473c96dd8570c137a5550f1e991f495fc2e2
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2019-04-19 12:02:33 +0530

    Added social links, copyright and functionality for dark mode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './main.scss';
+
+let root = document.documentElement;
+
+window.onload = () => {
+  root.style.setProperty('--font', window.localStorage.getItem('--font'));
+  root.style.setProperty('--bg', window.localStorage.getItem('--bg'));
+  if(window.localStorage.getItem('--bg') != 'white') {
+    document.getElementById('checkbox').checked = true;
+  }
+  console.log("---");
+};
+
+root.addEventListener('click', e => {
+  if(!document.getElementById('checkbox').checked) {
+    root.style.setProperty('--font', 'black');
+    root.style.setProperty('--bg', 'white');
+    window.localStorage.setItem('--font', 'black');
+    window.localStorage.setItem('--bg', 'white');
+  }
+  else {
+    root.style.setProperty('--font', 'white');
+    root.style.setProperty('--bg', '#181a1b');
+    window.localStorage.setItem('--font', 'white');
+    window.localStorage.setItem('--bg', '#181a1b');
+  }
+});
