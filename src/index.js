8f9eb3f876ef1b2f0d9661dc135a4011c4e7d87c
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2019-04-21 16:56:49 +0530

    Modal branch

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 import './main.scss';
 
-let root = document.documentElement;
+const root = document.documentElement;
 
 window.onload = () => {
-  root.style.setProperty('--font', window.localStorage.getItem('--font'));
-  root.style.setProperty('--bg', window.localStorage.getItem('--bg'));
   if(window.localStorage.getItem('--bg') != 'white') {
     document.getElementById('checkbox').checked = true;
+    let modal = document.getElementsByClassName('welcome-modal')[0];
+    console.log(modal)
+    modal.remove();
   }
+  root.addEventListener('click', e => {
+    if(!document.getElementById('checkbox').checked) {
+      root.style.setProperty('--font', 'black');
+      root.style.setProperty('--bg', 'white');
+      window.localStorage.setItem('--font', 'black');
+      window.localStorage.setItem('--bg', 'white');
+      let modal = document.getElementsByClassName('welcome-modal')[0];
+      modal.remove();
+    }
+    else {
+      root.style.setProperty('--font', 'white');
+      root.style.setProperty('--bg', '#181a1b');
+      window.localStorage.setItem('--font', 'white');
+      window.localStorage.setItem('--bg', '#181a1b');
+      
+    }
+  });
+  document.getElementById('closer').addEventListener('click', e => {
+    let modal = document.getElementsByClassName('welcome-modal')[0];
+    modal.style.display = 'none';
+    modal.remove();
+    window.scrollTo(0, 0);
+  });
+  root.style.setProperty('--font', window.localStorage.getItem('--font'));
+  root.style.setProperty('--bg', window.localStorage.getItem('--bg'));
   document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"https:\/\/use.fontawesome.com\/releases\/v5.8.1\/css\/all.css\" >");
   document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"https:\/\/fonts.googleapis.com\/css?family=Nunito+Sans\|Lato\" >");
+  window.scrollTo(0, 0);
 };
-
-root.addEventListener('click', e => {
-  if(!document.getElementById('checkbox').checked) {
-    root.style.setProperty('--font', 'black');
-    root.style.setProperty('--bg', 'white');
-    window.localStorage.setItem('--font', 'black');
-    window.localStorage.setItem('--bg', 'white');
-  }
-  else {
-    root.style.setProperty('--font', 'white');
-    root.style.setProperty('--bg', '#181a1b');
-    window.localStorage.setItem('--font', 'white');
-    window.localStorage.setItem('--bg', '#181a1b');
-  }
-});
+root.addEventListener('scroll', e => {if(window.localStorage.getItem('--bg') != 'white')document.getElementById('checkbox').checked = true;});
