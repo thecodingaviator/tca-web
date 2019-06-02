e4fbe386026887814197d54bb585936eeb89c1ad
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2019-06-02 11:07:22 +0100

    Fixed flipper and added todo

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 import './main.scss';
 
 let root = document.documentElement;
+let flipState = false
 
 window.onload = () => {
-  root.style.setProperty('--font', window.localStorage.getItem('--font'));
-  root.style.setProperty('--bg', window.localStorage.getItem('--bg'));
-  if(window.localStorage.getItem('--bg') != 'white') {
-    document.getElementById('checkbox').checked = true;
-    document.getElementById('dark-realm').innerHTML = 'Return to light! ¯\\_(ツ)_/¯';
-  }
+
+  // TODO
+  //Set styles from storage
+  // Change switch to dark more if dark
+
+  document.getElementById('checkbox').addEventListener('click', e => {
+    flipState = flipSwitch(flipState)
+  });
+
+  // Importing styles
   document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"https:\/\/use.fontawesome.com\/releases\/v5.8.1\/css\/all.css\" >");
   document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"https:\/\/fonts.googleapis.com\/css?family=Nunito+Sans\|Lato\" >");
 };
 
-root.addEventListener('click', e => {
-  if(!document.getElementById('checkbox').checked) {
-    root.style.setProperty('--font', 'black');
-    root.style.setProperty('--bg', 'white');
-    window.localStorage.setItem('--font', 'black');
-    window.localStorage.setItem('--bg', 'white');
-    document.getElementById('dark-realm').innerHTML = 'Flip me to enter the dark realm (▀̿Ĺ̯▀̿ ̿)';
+const flipSwitch = (flipState) => {
+  console.log(flipState)
+  if(flipState) { // If it is already black
+    flipState = !flipState; // Make it white
+    document.getElementById('checkbox').checked = false; // Undo checkbox
+    document.getElementById('dark-realm').innerHTML = 'Flip me to enter the dark realm (▀̿Ĺ̯▀̿ ̿)'; // Add dark text
+    root.style.setProperty('--font', 'black'); // Write up would be black
+    root.style.setProperty('--bg', 'white'); // Background would be white
   }
-  else {
-    root.style.setProperty('--font', 'white');
-    root.style.setProperty('--bg', '#181a1b');
-    window.localStorage.setItem('--font', 'white');
-    window.localStorage.setItem('--bg', '#181a1b');
-    document.getElementById('dark-realm').innerHTML = 'Return to light! ¯\\_(ツ)_/¯';
+  else { // If it is white
+    flipState = !flipState; // Make it black
+    document.getElementById('checkbox').checked = true; // Do checkbox
+    document.getElementById('dark-realm').innerHTML = 'Return to light! ¯\\_(ツ)_/¯'; // Add white text
+    root.style.setProperty('--font', 'white'); // Write up to be white
+    root.style.setProperty('--bg', 'black'); //  Background to be black
   }
-});
+  return flipState
+}
